b40d5f3285d46a8f2c2c5c0ae9b5083c2d1a195d
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-23 14:30:28 +0000

    dffd

--- main.cpp
+++ main.cpp
@@ -7,37 +7,28 @@
 #include <unistd.h>
 #define PORT 10002
 
+char* server_ip = "44.224.209.130" ;
+
 int main(int argc, char const* argv[])
 {
 	int status, valread, client_fd;
-	struct sockaddr_in serv_addr;
+	
 	char* login = u8"{ \"method\" : \"login\" , \"params\" : { \"login\" : \"49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g\" , \"pass\" : \"nor1\" , \"rigid\" : \"\" , \"agent\" : \"stratum-miner-py/0.1\" } , \"id\" : 1 }";
 	char buffer[3000] = { 0 };
   printf(login);
-	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("\n Socket creation error \n");
-		return -1;
-	}
 
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in serv_addr;
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
+  serv_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-	// Convert IPv4 and IPv6 addresses from text to binary
-	// form
-	if (inet_pton(AF_INET, "44.224.209.130", &serv_addr.sin_addr)
-		<= 0) {
-		printf(
-			"\nInvalid address/ Address not supported \n");
-		return -1;
-	}
-
-	if ((status
-		= connect(client_fd, (struct sockaddr*)&serv_addr,
-				sizeof(serv_addr)))
-		< 0) {
-		printf("\nConnection Failed \n");
-		return -1;
-	}
+  int suks = connect(client_fd,(sockaddr*) &serv_addr ,sizeof(serv_addr));
+  if (suks < 0){
+    printf("error in connecting");
+  }
+  
   printf("login\n");
 	send(client_fd, login, strlen(login), 0);
 	while(1==1){
