db9cb625973deabef30b886cb74947fae90a159c
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-24 11:16:48 +0000

    fgfg

---
+++ build/CMakeFiles/miner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miner.dir/main.cpp.o"
+  "miner"
+  "miner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
