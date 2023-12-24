5c9584af57d273483f13d63c96de2c00603446e7
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-24 11:54:07 +0000

    jh

--- main.cpp
+++ main.cpp
@@ -21,30 +21,44 @@ char* agent = u8"cccpminer";
 
 int main(int argc, char const* argv[])
 {
-	int status, valread, client_fd;
+	int status, valread, clientsock;
 	
-	char buffer[3000] = { 0 };
+	
   	
 
-	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	clientsock = socket(AF_INET, SOCK_STREAM, 0);
 
   	struct sockaddr_in serv_addr;
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
   	serv_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-	//init ends here
 
-	//basic json 
+	//init ends here
+	//conns
+	int suks = connect(clientsock,(sockaddr*) &serv_addr ,sizeof(serv_addr));
+
+  	if (suks < 0){
+    	printf("error in connecting");
+  	}
+	
+	nethandle(clientsock);
+
+	return 0;
+}
+
+
+int nethandle(int sockl){
+
+//basic json 
 	
 	rapidjson::Document loginjson;
 	loginjson.SetObject();
 	
 	loginjson.AddMember("method", "login", loginjson.GetAllocator());
 	
-	std::cout << "rashyet" << std::endl;
+	
 	loginjson.AddMember("id", 1, loginjson.GetAllocator());
-	std::cout <<"fsdf"<< std::endl ;
 	Value jsonparams(kObjectType);
 	
 	jsonparams.AddMember("login", Value(wallet, loginjson.GetAllocator()), loginjson.GetAllocator());
@@ -60,27 +74,30 @@ int main(int argc, char const* argv[])
 	std::string ulog = zbuffer.GetString();
 
 
-	//char* login = u8"{ \"method\" : \"login\" , \"params\" : { \"login\" : \"49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g\" , \"pass\" : \"nor1\" , \"rigid\" : \"\" , \"agent\" : \"cppminer\" } , \"id\" : 1 }\n";
 	char* login = (char*)ulog.c_str();
-	printf(login);
-	//basic json ends
-	//basic comm
-  	int suks = connect(client_fd,(sockaddr*) &serv_addr ,sizeof(serv_addr));
-
-  	if (suks < 0){
-    	printf("error in connecting");
-  	}
-
-  	printf("login\n");
 	strcat(login ,"\n");
-	send(client_fd, login, strlen(login), 0);
+	printf("login\n");
+	
+	//basic json ends
+	//response handler
+  	
+
+  	char recvbuffer[3000] = { 0 };
+	
+	send(sockl, login, strlen(login), 0);
 
 	while(1==1){
-	  recv(client_fd, &buffer,3000 ,0); 
-	  printf("%s\n", buffer);
-      memset(buffer, 0, sizeof(buffer));
+	  recv(sockl, &recvbuffer,3000 ,0); 
+	  printf("%s\n", recvbuffer);
+      memset(recvbuffer, 0, sizeof(recvbuffer));
     }
 	// closing the connected socket
-	close(client_fd);
+	close(sockl);
+
+
+
+
+
 	return 0;
+
 }
