45ac4482f2f75a4074c2af58ba3b2819f2fc3b4a
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-24 08:47:12 +0000

    ghgh

--- main.cpp
+++ main.cpp
@@ -39,9 +39,9 @@ int main(int argc, char const* argv[])
 	
 	rapidjson::Document loginjson;
 	
-	//loginjson.AddMember("method", "login", loginjson.GetAllocator());
+	loginjson.AddMember("method", "login", loginjson.GetAllocator());
 	std::cout << "rashyet" << std::endl;
-	//loginjson.AddMember("id", 1, loginjson.GetAllocator());
+	loginjson.AddMember("id", 1, loginjson.GetAllocator());
 	std::cout <<"fsdf"<< std::endl ;
 	Value jsonparams(kObjectType);
 	
