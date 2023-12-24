fda36cd1692f05e6b8865d3e11e1d8c05ddb9792
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-24 03:31:56 +0000

    hardcoding login

--- main.cpp
+++ main.cpp
@@ -15,22 +15,22 @@ int main(int argc, char const* argv[])
 	
 	char* login = u8"{ \"method\" : \"login\" , \"params\" : { \"login\" : \"49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g\" , \"pass\" : \"nor1\" , \"rigid\" : \"\" , \"agent\" : \"cppminer\" } , \"id\" : 1 }\n";
 	char buffer[3000] = { 0 };
-  printf(login);
+  	printf(login);
 
 	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-  struct sockaddr_in serv_addr;
+  	struct sockaddr_in serv_addr;
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
-  serv_addr.sin_addr.s_addr = inet_addr(server_ip);
+  	serv_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-  int suks = connect(client_fd,(sockaddr*) &serv_addr ,sizeof(serv_addr));
+  	int suks = connect(client_fd,(sockaddr*) &serv_addr ,sizeof(serv_addr));
 
-  if (suks < 0){
-    printf("error in connecting");
-  }
+  	if (suks < 0){
+    	printf("error in connecting");
+  	}
 
-  printf("login\n");
+  	printf("login\n");
 	send(client_fd, login, strlen(login), 0);
 	while(1==1){
 	  recv(client_fd, &buffer,3000 ,0); 
