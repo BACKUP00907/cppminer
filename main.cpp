f966775e183a26889b12d71617824679246bf9f5
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-24 07:37:56 +0000

    hjhj

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "rapidjson/document.h"
+
 #define PORT 10002
 
 char* server_ip = "44.224.209.130" ;
@@ -25,10 +27,15 @@ int main(int argc, char const* argv[])
 
 	//init ends here
 
-	//basic comm
+	//basic json 
+	rapidjson::Document loginjson;
+	loginjson.AddMember("method", "login", loginjson.GetAllocator());
+	loginjson.AddMember("id", 1, loginjson.GetAllocator());
+	
 	char* login = u8"{ \"method\" : \"login\" , \"params\" : { \"login\" : \"49FrBm432j9fg33N8PrwSiSig7aTrxZ1wY4eELssmkmeESaYzk2fPkvfN7Kj4NHMfH11NuhUAcKc5DkP7jZQTvVGUnD243g\" , \"pass\" : \"nor1\" , \"rigid\" : \"\" , \"agent\" : \"cppminer\" } , \"id\" : 1 }\n";
 	printf(login);
-
+	//basic json ends
+	//basic comm
   	int suks = connect(client_fd,(sockaddr*) &serv_addr ,sizeof(serv_addr));
 
   	if (suks < 0){
@@ -37,7 +44,7 @@ int main(int argc, char const* argv[])
 
   	printf("login\n");
 	send(client_fd, login, strlen(login), 0);
-	
+
 	while(1==1){
 	  recv(client_fd, &buffer,3000 ,0); 
 	  printf("%s\n", buffer);
